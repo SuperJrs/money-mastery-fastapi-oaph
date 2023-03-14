ba964dde18f8a24f667c71ddcb8021378285ebc8
¨ozielhumasi¨ <ozielhumasi@gmail.com>
2023-03-14 18:49:45 -0300

    refact: implementada camada repository. #6-#7-#8

--- controllers/conta.py
+++ controllers/conta.py
@@ -11,72 +11,32 @@ repo = ContaRepository()
 @router.get("/")
 def obter_contas(db = Depends(get_session)):
     result = repo.get_all(db)
-    print(result)
     return result
 
 # Alterar uma conta específica
 @router.put("/{cpf}")
-def alterar_dados_conta(cpf: int,conta_atualizada: ContaSchemaOptional , db=Depends(get_session)):
-
-    # Implementar a mesma lógica utilizada pelo Samy 
-    # Fazer um "for" para deixar o código mais apresentável
-    conta_a_alterar = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
-    if conta_atualizada.senha != None:
-        conta_a_alterar.senha = conta_atualizada.senha
-    if conta_atualizada.nome_proprietario != None:
-        print("Executado")
-        conta_a_alterar.nome_proprietario = conta_atualizada.nome_proprietario
-    if conta_atualizada.email != None:
-        conta_a_alterar.email = conta_atualizada.email
-    if conta_atualizada.telefone != None:
-        conta_a_alterar.telefone = conta_atualizada.telefone
-
-    db.commit()
-    return f'A conta de cpf {cpf} foi alterar com sucesso!  '
+def update_account(cpf: int,conta_atualizada: ContaSchemaOptional , db=Depends(get_session)):
+    result = repo.update_account(cpf, conta_atualizada, db)
+    return result
 
 
 # Criar uma conta
 @router.post("/")
 def criar_conta(nova_conta:ContaSchema, db=Depends(get_session)):
-    db.add(Conta(
-        cpf_proprietario = nova_conta.cpf_proprietario,
-        nome_proprietario = nova_conta.nome_proprietario,
-        dt_nasc_proprietario = nova_conta.dt_nasc_proprietario,
-        telefone = nova_conta.telefone,
-        email = nova_conta.email,
-        senha = nova_conta.senha
-    ))
-    db.commit()
-    return {
-        "detail": "Conta criada com sucesso!"
-    }
+    new_conta = repo.create_conta(nova_conta, db)
+    return new_conta
+
 
 # Pegar uma conta específica
 @router.get("/{cpf}", response_model=ContaSchema)
 def pegar_conta(cpf: int , db=Depends(get_session)):
-    conta = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
-
-    if not conta:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Em nosso sistema, não há uma conta cadastrada para o cpf {cpf}'
-        )
-    return conta
-
-
+    result = repo.get_conta(cpf, db)
+    return result
 
 # Remover uma conta
 @router.delete("/{cpf}")
 def remover_conta(cpf: int, db=Depends(get_session)):
-    conta_a_remover = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
-
-    if not conta_a_remover:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Conta não existe!"
-        )
-    db.delete(conta_a_remover)
-    db.commit()
-    return f'A conta {cpf} foi removida com sucesso!'
+    result = repo.delete_account(cpf, db)
+    return result
 
 
--- repository/conta_repository.py
+++ repository/conta_repository.py
@@ -1,10 +1,68 @@
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from models.conta_model import Conta
-from schemas.conta_schema import ContaSchema
+from schemas.conta_schema import ContaSchema, ContaSchemaOptional
 
 class ContaRepository:
+
+    # Implementar a mesma lógica utilizada pelo Samy 
+    # Fazer um "for" para deixar o código mais apresentável
+
+    @staticmethod
+    def update_account(cpf:int, updated_account:ContaSchemaOptional, db:Session):
+        conta_a_alterar = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
+        if updated_account.senha != None:
+            conta_a_alterar.senha = updated_account.senha
+        if updated_account.nome_proprietario != None:
+            print("Executado")
+            conta_a_alterar.nome_proprietario = updated_account.nome_proprietario
+        if updated_account.email != None:
+            conta_a_alterar.email = updated_account.email
+        if updated_account.telefone != None:
+            conta_a_alterar.telefone = updated_account.telefone
+
+        db.commit()
+        return f'A conta de cpf {cpf} foi alterar com sucesso!'
+
+
+    @staticmethod
+    def delete_account(cpf:int, db:Session):
+        conta_a_remover = ContaRepository.get_conta(cpf, db)
+
+        if not conta_a_remover:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Conta não existe!"
+            )
+        db.delete(conta_a_remover)
+        db.commit()
+        return f"A conta de cpf {cpf} foi removida com sucesso!"
     
+    @staticmethod
+    def get_conta(cpf: int, db:Session):
+        conta = db.query(Conta).filter(Conta.cpf_proprietario == cpf).first()
+
+        if not conta:
+            raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Em nosso sistema, não há uma conta cadastrada para o cpf {cpf}'
+        )
+        return conta
+
+    @staticmethod
+    def create_conta(nova_conta:ContaSchema, db:Session):
+        db.add(Conta(
+            cpf_proprietario = nova_conta.cpf_proprietario,
+            nome_proprietario = nova_conta.nome_proprietario,
+            dt_nasc_proprietario = nova_conta.dt_nasc_proprietario,
+            telefone = nova_conta.telefone,
+            email = nova_conta.email,
+            senha = nova_conta.senha
+        ))
+        db.commit()
+        return {
+            "detail": "Conta criada com sucesso!"
+        }
 
     @staticmethod
     def get_all(db:Session):
